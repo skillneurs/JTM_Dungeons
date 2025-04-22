eda6d4063be9e5f3b82626e3873eadfd82bdb790
skillneurs <skillneurs@gmail.com>
2025-04-22 15:13:17 +0200

    menu

---
+++ JTM-donjon/GameJS/gamedisplay.js
@@ -0,0 +1,18 @@
+const start = document.querySelector("#closePopup")
+const popup = document.getElementById("popup")
+const canvas = document.querySelector('#renderCanvas')
+let code = document.getElementById("codeBefore")
+
+
+start.addEventListener("click", function () {
+   popup.style.display="none"
+   canvas.style.display = 'block'
+})
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+        popup.style.display = "flex";
+        canvas.style.display = 'none';
+        
+    }
+});
--- JTM-donjon/Javascript/display.js
+++ JTM-donjon/Javascript/display.js
@@ -1,21 +1,10 @@
-const start = document.querySelector("#closePopup")
-const popup = document.getElementById("popup")
-const canvas = document.querySelector('#renderCanvas')
-let code = document.getElementById("codeBefore")
+const menu = document.querySelector("#menu");
+const menuitem = document.querySelector(".menuItem");
 
-
-start.addEventListener("click", function () {
-   popup.style.display="none"
-   canvas.style.display = 'block'
-})
-
-document.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
-        popup.style.display = "flex";
-        canvas.style.display = 'none';
-        
-    }
+menu.addEventListener("click", function () {
+  if (menuitem.style.display == "none") {
+    menuitem.style.display = "block";
+  } else {
+    menuitem.style.display = "none";
+  }
 });
-
-
-
