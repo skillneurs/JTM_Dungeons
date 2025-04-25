0ddf0279f9ec1d9580293121606d915643c9c2c9
skillneurs <skillneurs@gmail.com>
2025-04-25 10:26:53 +0200

    popup enigme final

--- JTM-donjon/Javascript/cave.js
+++ JTM-donjon/Javascript/cave.js
@@ -1,6 +1,11 @@
 const btnMarcher = document.getElementById("btnMarcher")
+const btncourir = document.getElementById("btncourir")
+const btnmarcher = document.getElementById("btnmarcher")
 const popupEnigme = document.querySelector(".popupEnigme")
 
 btnMarcher.addEventListener("click", function () {
     popupEnigme.classList.remove("displayNone");
+    btncourir.classList.add("displayNone");
+    btnmarcher.classList.add("displayNone");
+
 });
