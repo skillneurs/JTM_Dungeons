612a70cc3c6ede579841681b403bd9a09cadb24f
skillneurs <skillneurs@gmail.com>
2025-04-25 10:59:43 +0200

    class

--- JTM-donjon/Javascript/garde.js
+++ JTM-donjon/Javascript/garde.js
@@ -21,7 +21,7 @@ const qst1 = document.getElementById("qst1")
 const qst2 = document.getElementById("qst2")
 const qst3 = document.getElementById("qst3")
 const qst4 = document.getElementById("qst4")
-const btn = document.querySelector("boutonok")
+const btn = document.querySelector(".boutonok")
 
 btn.addEventListener("click", function(){
     if (qst1.value === "yes") {
