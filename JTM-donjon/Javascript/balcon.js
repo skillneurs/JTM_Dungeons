018ce7b15c9acf12aa96effbf45fd0cb82a96c08
Zatho0 <thom.reix4@outlook.com>
2025-04-26 15:20:12 +0200

    Add quiz functionality and restructure HTML; update CSS for quiz styles

--- JTM-donjon/Javascript/balcon.js
+++ JTM-donjon/Javascript/balcon.js
@@ -11,8 +11,6 @@ const popuplose4 = document.querySelector(".popuplose4");
 btescalader2.addEventListener("click", function () {
     if (popuplose4) {
         popuplose4.classList.remove("none4");
-        console.log("Classe 'none' retirée !");
-    } else {
-        console.log("L'élément popuplose n'a pas été trouvé !");
-    }
+        
+    } 
 });
